ce8e2059474c5011e4cdeb64ace0734d2f23fbcf
Julia Ivanova <for.julia96@gmail.com>
2022-05-14 18:54:45 +0300

    First page

--- src/main/java/spring/mvc/HomeController.java
+++ src/main/java/spring/mvc/HomeController.java
@@ -1,2 +1,13 @@
-package spring.mvc;public class HomeController {
+package spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showPage () {
+        return "main-menu";
+    }
 }
