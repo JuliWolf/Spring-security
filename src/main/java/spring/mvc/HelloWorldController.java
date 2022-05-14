e666e7289dab1cb54de80d4c3a812be44d93724e
Julia Ivanova <for.julia96@gmail.com>
2022-05-14 19:27:27 +0300

    Send data to model

--- src/main/java/spring/mvc/HelloWorldController.java
+++ src/main/java/spring/mvc/HelloWorldController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
 @Controller
 public class HelloWorldController {
 
@@ -18,6 +21,26 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm (@RequestParam(name = "studentName") String studentName, Model model) {
         model.addAttribute("studentName", studentName);
+        return "helloworld";
+    }
+
+    // new a controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        // read the request parameter from HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("studentName", theName);
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
