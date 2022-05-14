98b78ca3ef1617987a106cc65d871e5cacc8a19a
Julia Ivanova <for.julia96@gmail.com>
2022-05-14 19:19:59 +0300

    Hello world pages

---
+++ src/main/java/spring/mvc/HelloWorldController.java
@@ -0,0 +1,23 @@
+package spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    // need a controller method to show the initial HTML form
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    // need a controller method to process the HTML form
+    @RequestMapping("/processForm")
+    public String processForm (@RequestParam(name = "studentName") String studentName, Model model) {
+        model.addAttribute("studentName", studentName);
+        return "helloworld";
+    }
+}
