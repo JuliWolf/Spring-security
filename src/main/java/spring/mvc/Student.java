5b98722ba713937b6c46d0b0e4c62fd9ac7935f3
Julia Ivanova <for.julia96@gmail.com>
2022-05-15 21:32:49 +0300

    Get options from class

--- src/main/java/spring/mvc/Student.java
+++ src/main/java/spring/mvc/Student.java
@@ -1,5 +1,7 @@
 package spring.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
@@ -8,7 +10,18 @@ public class Student {
 
     private String country;
 
-    public Student () {}
+    private LinkedHashMap<String, String> countryOptions;
+
+    public Student () {
+        // populate country options: used ISO country code
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("RU", "Russia");
+    }
 
     public String getLastName() {
         return lastName;
@@ -33,5 +46,9 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
     }
 }
