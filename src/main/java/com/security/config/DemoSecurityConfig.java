7f50049dc32152fdd4ca27a690be3ff38ec47160
Julia Ivanova <for.julia96@gmail.com>
2022-08-03 23:00:49 +0300

    Custom login page

--- src/main/java/com/security/config/DemoSecurityConfig.java
+++ src/main/java/com/security/config/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +21,17 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
         .withUser("mary").password(passwordEncoder().encode("test123")).roles("MANAGER")
         .and()
         .withUser("susan").password(passwordEncoder().encode("test123")).roles("ADMIN");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+          .loginPage("/login")
+          .loginProcessingUrl("/authenticateTheUser")
+          .permitAll();
   }
 
   @Bean
